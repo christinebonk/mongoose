0ee36fccc93546fd8ff76667366e9ca7825ffd45
Christine Bonk <christinebonk@gmail.com>
2018-08-26 18:36:45 -0400

    fixed issue?

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 var request = require("request");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 var routes = require("./controllers/controllers.js");
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-app.listen(process.env.PORT || PORT, function() {
+app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
 
