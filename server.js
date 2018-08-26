0bd8db15543757df559d846399c16344eaffef26
Christine Bonk <christinebonk@gmail.com>
2018-08-26 18:38:41 -0400

    added code

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var app = express();
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 var routes = require("./controllers/controllers.js");
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 routes(app);
@@ -17,8 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
